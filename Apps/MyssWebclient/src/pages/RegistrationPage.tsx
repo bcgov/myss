ff1a0a54d3a8807128c641346ac1fde2ab47be8f
sinan soykut <sinan.soykut@gov.bc.ca>
2026-09-10 11:44:08 -0700

    formatting syntax to make it unifrom

--- Apps/MyssWebclient/src/pages/RegistrationPage.tsx
+++ Apps/MyssWebclient/src/pages/RegistrationPage.tsx
@@ -1,14 +1,12 @@
-import styles from "./RegistrationPage.module.css";
 import { Link } from "react-router";
-
+import styles from "./RegistrationPage.module.css";
 
 export default function RegistrationPage() {
     return (
         <div className={styles.page}>
             <nav aria-label="Breadcrumb">
-        <Link to="/">← Home</Link>
-        
-        </nav>
+                <Link to="/">← Home</Link>      
+            </nav>
             <h1>Registration</h1>
         </div>
     );
